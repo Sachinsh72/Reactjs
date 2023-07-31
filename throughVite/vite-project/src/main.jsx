e5512d0bc7ba35986c1d742a74605ddc9206e63f
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-07-31 13:52:47 +0530

    Problem statement Assignment

--- throughVite/vite-project/src/main.jsx
+++ throughVite/vite-project/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { Person, Header, List } from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Person name="Sachin" age="23" />
+    <Header title ="Problem 3"/>
+    <List item="Item1"/>
+    <List item="Item2"/>
+    {/* <Button text="Click to chenge" /> */}
   </React.StrictMode>,
 )
