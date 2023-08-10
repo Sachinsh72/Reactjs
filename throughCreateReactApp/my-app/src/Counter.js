f5c3cc910193dd5de7b35fb49f117a50e97cd210
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-08-10 16:29:22 +0530

    useState hook

--- throughCreateReactApp/my-app/src/Counter.js
+++ throughCreateReactApp/my-app/src/Counter.js
@@ -7,6 +7,8 @@ function Counter(){
     const [y, setY] = useState(0);
     // let z = 10; 
 
+    const [a, setA] = useState(0);
+
     return(
         <>
             Count Z: {z}
@@ -18,6 +20,11 @@ function Counter(){
             Count Y: {y}
             <button onClick={() => setY(y+1)}>Inc</button>
             <button onClick={() => setY(y-1)}>Dec</button>
+            <br/>
+            {/* One concept comes => conditional rendering */}
+            Count A: {a} is {(a % 2 == 0) ? 'Even' : 'Odd'}
+            <button onClick={() => setA(a+1)}>Inc</button>
+            <button onClick={() => setA(a-1)}>Dec</button>
         </>
     )
 
